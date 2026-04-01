982684208b4019b31c6de59cccac2ac9e92401ae
pancato <pancat.ooo@gmail.com>
2026-04-01 10:15:14 +0800

    feat: implement Clerk authentication sign-up flow with email verification

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,9 @@
 import { tabs } from "@/constants/data";
 import { colors, components } from "@/constants/theme";
+import { useAuth } from "@clerk/expo";
 // import { useAuth } from "@clerk/expo";
 import { clsx } from "clsx";
-import { Tabs } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
 import { Image, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
@@ -18,18 +19,18 @@ const TabIcon = ({ focused, icon }: TabIconProps) => {
   );
 };
 const TabLayout = () => {
-  //   const { isSignedIn, isLoaded } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
   const insets = useSafeAreaInsets();
 
   // Wait for auth to load before rendering anything
-  //   if (!isLoaded) {
-  //     return null;
-  //   }
+  if (!isLoaded) {
+    return null;
+  }
 
   // Redirect to sign-in if user is not authenticated
-  //   if (!isSignedIn) {
-  //     return <Redirect href="/(auth)/sign-in" />;
-  //   }
+  if (!isSignedIn) {
+    return <Redirect href="/(auth)/sign-in" />;
+  }
 
   return (
     <Tabs
